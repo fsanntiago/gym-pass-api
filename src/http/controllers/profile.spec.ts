ac89f01e449efdc56f97f88a59778be82691fa34
fsanntiago <zerofabricio@hotmail.com>
2024-05-19 16:50:54 -0300

    test(e2e): ensure get profile user

---
+++ src/http/controllers/profile.spec.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+
+describe('Profile (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John',
+      email: 'john@gmail.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'john@gmail.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({ email: 'john@gmail.com' }),
+    )
+  })
+})
