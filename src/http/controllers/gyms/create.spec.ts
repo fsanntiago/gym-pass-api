7939a1b84635a5c670742fb9a13240577a7a24b5
fsanntiago <zerofabricio@hotmail.com>
2024-05-23 10:24:18 -0300

    test(e2e): ensure gym creation

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '119999999',
+        longitude: -44.2295939,
+        latitude: -19.2953455,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
